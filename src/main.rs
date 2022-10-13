a6b81e62144187b64b720d6cadf55f1fbc57f320
Michele Memoli <michele@100shapes.com>
2022-10-13 22:22:17 +0100

    Added contribution_on method

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,14 @@ fn main() -> Result<()> {
         .name("Project 1")
         .start_date(NaiveDate::from_ymd(2022, 1, 1))
         .weeks(4)
+        .revenue(2000)
         .build();
 
     let t2: Timeline = Timeline::builder()
         .name("Project 2")
         .start_date(NaiveDate::from_ymd(2022, 5, 20))
         .weeks(4)
+        .revenue(3000)
         .build();
 
     let timelines = vec![t1, t2];
--- src/timelines.rs
+++ src/timelines.rs
@@ -18,8 +18,17 @@ impl Timeline {
 
     /// Returns the end date of this [`Timeline`].
     #[allow(dead_code)]
-    fn end_date(&self) -> NaiveDate {
+    pub fn end_date(&self) -> NaiveDate {
         self.start_date.add(self.duration)
+    }
+
+    /// Returns the value of this metric on a given date.
+    pub fn contribution_on(&self, date: NaiveDate) -> u32 {
+        let in_scope = date >= self.start_date && date <= self.end_date();
+        match in_scope {
+            true => self.metric.value,
+            false => 0,
+        }
     }
 }
 
@@ -27,6 +36,7 @@ pub struct TimelineBuilder {
     name: String,
     start_date: NaiveDate,
     duration: Duration,
+    metric: Revenue,
 }
 
 impl TimelineBuilder {
@@ -35,6 +45,7 @@ impl TimelineBuilder {
             name: "New Timeline".to_owned(),
             start_date: NaiveDate::default(),
             duration: Duration::weeks(4),
+            metric: Revenue { value: 2000 },
         }
     }
 
@@ -43,12 +54,17 @@ impl TimelineBuilder {
             name: self.name,
             start_date: self.start_date,
             duration: self.duration,
-            metric: Revenue { value: 2000 },
+            metric: self.metric,
         }
     }
 
     pub fn name(mut self, name: &str) -> TimelineBuilder {
         self.name = name.into();
+        self
+    }
+
+    pub fn revenue(mut self, revenue: u32) -> TimelineBuilder {
+        self.metric = Revenue { value: revenue };
         self
     }
 
@@ -105,5 +121,21 @@ mod tests {
         };
         let t_from_builder = TimelineBuilder::new().build();
         assert_eq!(t, t_from_builder);
+    }
+
+    #[test]
+    fn test_contribution_on() {
+        let t = Timeline {
+            name: "New Timeline".to_string(),
+            start_date: NaiveDate::from_ymd(2022, 1, 2),
+            duration: Duration::weeks(4),
+            metric: Revenue { value: 2000 },
+        };
+        assert_eq!(t.contribution_on(NaiveDate::from_ymd(2022, 1, 1)), 0);
+        assert_eq!(t.contribution_on(NaiveDate::from_ymd(2022, 1, 2)), 2000);
+        assert_eq!(t.contribution_on(NaiveDate::from_ymd(2022, 1, 3)), 2000);
+        assert_eq!(t.contribution_on(NaiveDate::from_ymd(2022, 1, 30)), 2000);
+        assert_eq!(t.contribution_on(NaiveDate::from_ymd(2022, 1, 31)), 0);
+        assert_eq!(t.contribution_on(NaiveDate::from_ymd(2022, 2, 1)), 0);
     }
 }
